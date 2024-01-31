c72e853c773bc80b62c1ca75bfa69e0f2b2edc4a
iamlukovkin <iamlukovkin@gmail.com>
2024-01-31 23:04:11 +0300

    ПП-ЛР3/Т.2

--- Промышленное програмирование/ЛР/Lab3/src/App.java
+++ Промышленное програмирование/ЛР/Lab3/src/App.java
@@ -1,10 +1,19 @@
-public class App {
+public class App 
+{
     public static void main(String[] args) 
     {
         task1();    
+        task2();
     }
 
-    public static void task1() {
+    /**
+     * Patient: id, Фамилия, Имя, Отчество, Адрес, Телефон, Номер медицинской карты, Диагноз. 
+     * Создать массив объектов. Вывести: 
+     * a) список пациентов, имеющих данный диагноз; 
+     * b) список пациентов, номер медицинской карты которых находится в заданном интервале. 
+     */
+    public static void task1() 
+    {
         Patient[] patients = new Patient[10];
         String[] surnames = {
             "Иванов", "Петров", "Сидоров", "Кузнецов", "Смирнов", 
@@ -35,7 +44,8 @@ public class App {
             "Коронавирус", "ОРВИ", "Грипп", "Туберкулез", "Гастрит"
         };
 
-        for (int i = 0; i < patients.length; i++) {
+        for (int i = 0; i < patients.length; i++) 
+        {
             patients[i] = new Patient
             (
                 surnames[i], names[i], lastnames[i],
@@ -46,17 +56,35 @@ public class App {
         String requiredDiagnosis = "ОРВИ";
         System.out.println("Пациенты с диагнозом " + requiredDiagnosis + ":\n");
 
-        for (Patient patient : patients) {
+        for (Patient patient : patients) 
+        {
             if (patient.get_Diagnosis().equals(requiredDiagnosis)) 
                 System.out.println(patient.toString());
         }
 
         System.out.println("\nПациенты с номером медицинской карты в диапазоне от 20000 до 40000:\n");
 
-        for (Patient patient : patients) {
+        for (Patient patient : patients) 
+        {
             int medCard = patient.get_MedCard();
             if (medCard >= 20000 && medCard <= 40000) 
                 System.out.println(patient.toString());
         }
+    }
+    
+    /**
+     * Определить класс Полином степени n. 
+     * Объявить массив/список/множество из m полиномов и определить сумму полиномов массива. 
+     */
+    public static void task2()
+    {
+        Polynom p1 = new Polynom(3, new int[] {1, 2, 3, 4});
+        Polynom p2 = new Polynom(2, new int[] {1, 2, 3});
+        Polynom p3 = new Polynom(1, new int[] {1, 2});
+        
+        Polynom[] polynoms = {p1, p2, p3};
+        Polynom sum = Polynom.sumPolynomals(polynoms);
+
+        System.out.println("Сумма полиномов: " + sum.toString());
     }
 }
